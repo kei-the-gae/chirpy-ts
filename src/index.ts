292c9cd21663d52e51d3dd3401c64168893ab223
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-05-13 16:28:04 -0700

    add chirp validation

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
   middlewareLogResponse,
   middlewareMetricsInc,
 } from "./api/middleware.js";
+import { handlerChirpsValidate } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
@@ -17,6 +18,8 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerReset);
+
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
