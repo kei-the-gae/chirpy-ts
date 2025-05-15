a660f46b882f1be5a7152bd2f1b8d4275e36b4b2
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-05-15 01:00:38 -0700

    add jwts

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,10 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
+
+import { UserNotAuthenticatedError } from "./api/errors.js";
+
+const TOKEN_ISSUER = "chirpy";
 
 export async function hashPassword(password: string) {
   const saltRounds = 10;
@@ -7,4 +13,42 @@ export async function hashPassword(password: string) {
 
 export async function checkPasswordHash(password: string, hash: string) {
   return bcrypt.compare(password, hash);
+}
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export function makeJWT(userID: string, expiresIn: number, secret: string) {
+  const issuedAt = Math.floor(Date.now() / 1000);
+  const expiresAt = issuedAt + expiresIn;
+  const token = jwt.sign(
+    {
+      iss: TOKEN_ISSUER,
+      sub: userID,
+      iat: issuedAt,
+      exp: expiresAt,
+    } satisfies payload,
+    secret,
+    { algorithm: "HS256" },
+  );
+
+  return token;
+}
+
+export function validateJWT(tokenString: string, secret: string) {
+  let decoded: payload;
+  try {
+    decoded = jwt.verify(tokenString, secret) as JwtPayload;
+  } catch (e) {
+    throw new UserNotAuthenticatedError("Invalid token");
+  }
+
+  if (decoded.iss !== TOKEN_ISSUER) {
+    throw new UserNotAuthenticatedError("Invalid issuer");
+  }
+
+  if (!decoded.sub) {
+    throw new UserNotAuthenticatedError("No user ID in token");
+  }
+
+  return decoded.sub;
 }
